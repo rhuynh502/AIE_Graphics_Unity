60b67eb752a545c8ff616d094191f6a9320fd54b
rhuynh502 <98858999+rhuynh502@users.noreply.github.com>
2023-04-18 16:59:25 +1000

    Added animations to blend tree

--- URP/Assets/Scripts/Animation View/AnimationTesting.cs
+++ URP/Assets/Scripts/Animation View/AnimationTesting.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,29 +8,66 @@ public class AnimationTesting : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Slider sliderForward;
+    [SerializeField] private Slider sliderRight;
+    [SerializeField] private Slider sliderZoom;
+    [SerializeField] private SkinnedMeshRenderer skinnedMeshRenderer;
+    
+    private float blinkTime = 0;
+    private bool canBlink = true;
+    private new Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
+    }
+
+    void Update()
+    {
+        if (blinkTime > 0.35f)
+        {
+            blinkTime = 0;
+            StartCoroutine(Blink_CR());
+        }
+
+        if(canBlink)
+            blinkTime += Time.deltaTime;
     }
 
     public void WalkForward()
     {
-        animator.SetFloat("Forward", sliderForward.value);
+        animator.SetFloat("Forward", sliderForward.value / 10);
+    }
+
+    public void WalkRight()
+    {
+        animator.SetFloat("Right", sliderRight.value / 10);
     }
 
     public void Jump()
     {
         animator.SetTrigger("Jump");
-        /*if(!animator.GetBool("Jump"))
-            StartCoroutine(JumpCooldown_CR());*/
     }
 
-    IEnumerator JumpCooldown_CR()
+    public void Zoom()
     {
-        animator.SetBool("Jump", true);
-        yield return new WaitForSeconds(2);
-        animator.SetBool("Jump", false);
+        camera.transform.Translate(new float3(0, 0, sliderZoom.value));
+    }
+
+    IEnumerator Blink_CR()
+    {
+        canBlink = false;
+        skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Lerp(0, 100, blinkTime / 0.35f));
+        yield return new WaitForSeconds(8);
+        skinnedMeshRenderer.SetBlendShapeWeight(0, Mathf.Lerp(100, 0, blinkTime / 0.35f));
+        canBlink = true;
+    }
+
+    private void ResetFacial()
+    {
+        for(int i = 6; i < 10; i++)
+        {
+            skinnedMeshRenderer.SetBlendShapeWeight(i, 0);
+        }
     }
 }
