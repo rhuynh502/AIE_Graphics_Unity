f373c7a89fc7c4b3a5976fc8c1dfaf739e658d96
Ryan <rhuynh502@gmail.com>
2023-04-26 12:35:10 +1000

    Added new naimation and fixed jump

--- URP/Assets/Scripts/Player.cs
+++ URP/Assets/Scripts/Player.cs
@@ -26,7 +26,7 @@ public class Player : MonoBehaviour
     private Vector2 _direction;
     private bool _isMoving;
     private bool _isSprinting;
-    private bool _isAiming;
+    private bool _isWaving;
 
     // Private fields
     private float _speed;
@@ -35,14 +35,16 @@ public class Player : MonoBehaviour
     private Vector3 _aimTarget;
     private Vector3 _rotation = Vector3.zero;
     private Vector3 _movement;
+    private bool _isJumping;
+    private float _jumpVelocity = 0;
 
     // Public fields
     public float movementSpeed = 1f;
-    public float runSpeed = 3f;
+    public float runSpeed = 2f;
     public float smoothTime = 1f;
     public float rotationSpeed = 10f;
     public bool isCursorLocked = true;
-
+    public Vector2 gravity = new Vector2(0, -9.8f);
 
     // Start is called before the first frame update
     void Awake()
@@ -60,13 +62,18 @@ public class Player : MonoBehaviour
     public void OnMove(InputAction.CallbackContext _value)
     {
         _direction = _value.ReadValue<Vector2>();
-        Debug.Log("Move: " + _direction);
         if (_direction == Vector2.zero)
             _isMoving = false;
         else
             _isMoving = true;
         _movement.x = _direction.x;
         _movement.z = _direction.y;
+    }
+
+    public void OnWave(InputAction.CallbackContext _value)
+    {
+        _isWaving = _value.ReadValueAsButton();
+        _animator.SetTrigger("IsWaving");
     }
 
     public void OnSprint(InputAction.CallbackContext _value)
@@ -78,29 +85,32 @@ public class Player : MonoBehaviour
         else if(_value.canceled)
             _isSprinting = false;
     }
-    
-    public void OnAim(InputAction.CallbackContext _value)
+
+    public void OnJump(InputAction.CallbackContext _value)
     {
-        _isAiming = _value.ReadValueAsButton();
+        if (_value.started && _characterController.isGrounded)
+        {
+            _animator.SetBool("Jumping", true);
+            _jumpVelocity = Mathf.Sqrt(-2 * Physics.gravity.y);
+        }
+        else if (_value.canceled)
+            _animator.SetBool("Jumping", false);
     }
 
     private void Update()
     {
-        if(!_isAiming)
+        if(!_isWaving)
         {
-            CasualMovement(Time.fixedDeltaTime);
+            //CasualMovement(Time.fixedDeltaTime);
+            SetAnimationActiveLayer(_animator, 0, 0, Time.deltaTime, 10);
             SetAnimationActiveLayer(_animator, 1, 0, Time.deltaTime, 10);
-            //SetAnimationActiveLayer(_animator, 2, 0, Time.deltaTime, 10);
-            _virtualCamera.Priority = 9;
-
         }
         else
         {
+            SetAnimationActiveLayer(_animator, 0, 1, Time.deltaTime, 10);
             SetAnimationActiveLayer(_animator, 1, 1, Time.deltaTime, 10);
-            //SetAnimationActiveLayer(_animator, 2, 1, Time.deltaTime, 10);
-            _virtualCamera.Priority = 11;
-
         }
+
     }
 
     // Update is called once per frame
@@ -108,23 +118,25 @@ public class Player : MonoBehaviour
     {
         _speed = _isSprinting ? runSpeed : !_isMoving ? 0 : movementSpeed;
 
-        _playerVelocity = Vector2.SmoothDamp(_playerVelocity, _direction, ref _smoothedPlayerVelocity, smoothTime);
+        _playerVelocity = Vector2.SmoothDamp(_playerVelocity, _direction * _speed, ref _smoothedPlayerVelocity, smoothTime);
 
         Vector3 move = new Vector3(_playerVelocity.x, 0, _playerVelocity.y);
         move = move.x * _camera.transform.right.normalized + move.z * _camera.transform.forward.normalized;
-        move.y = 0;
+        move.y = _jumpVelocity;
 
-        _characterController.Move(move * Time.fixedDeltaTime * _speed);
-        
+        _characterController.Move(move * Time.fixedDeltaTime);
+
+        _jumpVelocity += gravity.y * Time.deltaTime;
+
         Quaternion rotation = Quaternion.Euler(0, _camera.transform.eulerAngles.y, 0);
         transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.fixedDeltaTime * rotationSpeed);
-        
+
     }
 
     private void LateUpdate()
     {
-        _animator.SetFloat("Forward", Mathf.Lerp(_playerVelocity.y, _playerVelocity.y * (_isSprinting ? runSpeed : _speed), Time.deltaTime * 50));
-        _animator.SetFloat("Right", _playerVelocity.x * (_isSprinting ? runSpeed : _speed));
+        _animator.SetFloat("Forward", _playerVelocity.y);
+        _animator.SetFloat("Right", _playerVelocity.x);
 
         _animator.SetBool("IsMoving", _isMoving);
 
@@ -165,5 +177,6 @@ public class Player : MonoBehaviour
         transform.forward = new Vector3(transform.forward.x + input.x, 0, transform.forward.z + input.y);
         transform.rotation = Quaternion.Euler(0, _rotation.x, 0);
         _followTransform.localRotation = Quaternion.Euler(_rotation.y, 0, 0);
+
     }
 }
